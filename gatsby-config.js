cec06b7169e4295fe8836b36ed5d267d55dc520b
stefan judis <stefanjudis@gmail.com>
2018-02-19 19:24:35 +0100

    Add preview API functionality

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,15 @@
+const contentfulConfig = require('./.contentful')
+
 module.exports = {
   pathPrefix: '/gatsby-contentful-starter',
   plugins: [
-    `gatsby-transformer-remark`,
-    `gatsby-plugin-react-helmet`,
+    'gatsby-transformer-remark',
+    'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-source-contentful`,
-      options: {
-        spaceId: `cmls7salv7po`,
-        accessToken: `2da5f408b0933f849521c3650cc4e124415fb59d7cd59ea111b09c677af757c0`,
-      },
+      resolve: 'gatsby-source-contentful',
+      options: process.env.NODE_ENV === 'development' ?
+        contentfulConfig.development :
+        contentfulConfig.production
     },
   ],
 }
