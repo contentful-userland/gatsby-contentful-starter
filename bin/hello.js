d7008236bffef559f1fcbfe9060c14b1a0a4cade
stefan judis <stefanjudis@gmail.com>
2018-02-20 16:20:06 +0100

    Rename setup script

--- bin/hello.js
+++ bin/hello.js
@@ -8,7 +8,7 @@ ${chalk.green('Hey there! 👋')}
 Thanks for giving the ${pkg.name} a try. 🎉
 To get you going really quickly this project includes a setup step.
 
-${chalk.yellow.bold('npm run import')} automates the following steps for you:
+${chalk.yellow.bold('npm run setup')} automates the following steps for you:
   - creates a config file ${chalk.yellow('./.contentful.json')}
   - imports ${chalk.green('a predefined content model')}
 
