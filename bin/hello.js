488d265d35ce0283eb8e40bbbcdc1114fa88bfb4
stefan judis <stefanjudis@gmail.com>
2018-02-20 12:45:22 +0100

    Add postinstall intro message

---
+++ bin/hello.js
@@ -0,0 +1,17 @@
+const chalk = require('chalk')
+const pkg = require('../package.json')
+
+console.log(`
+****************************
+
+${chalk.green('Hey there! 👋')}
+
+Thanks for giving the ${pkg.name} a try. 🎉
+To get you going really quickly this project includes a setup step.
+
+${chalk.yellow('npm run import')} automates the following steps for you:
+  - imports ${chalk.green('a predefined content model')}
+  - creates a config file ${chalk.yellow('.contentful.json')}
+
+When this is done run ${chalk.yellow('npm run dev')} and you're good to go.
+`);
