45b1786c9c663cf8bacd38075b9037166bebe55f
stefan judis <stefanjudis@gmail.com>
2018-02-20 15:04:54 +0100

    Improve intro message

--- bin/hello.js
+++ bin/hello.js
@@ -9,9 +9,22 @@ Thanks for giving the ${pkg.name} a try. 🎉
 To get you going really quickly this project includes a setup step.
 
 ${chalk.yellow.bold('npm run import')} automates the following steps for you:
-  - creates a config file ${chalk.yellow('.contentful.json')}
+  - creates a config file ${chalk.yellow('./.contentful.json')}
   - imports ${chalk.green('a predefined content model')}
 
-When this is done run ${chalk.yellow('npm run dev')} to start the development environment.
+When this is done run:
+
+${chalk.yellow(
+  'npm run dev'
+)} to start a development environment at ${chalk.green('localhost:8000')}
+
+or
+
+${chalk.yellow(
+  'npm run build'
+)} to create a production ready static site in ${chalk.green('./public')}
+
+For further information check the readme of the project
+(https://github.com/contentful-userland/gatsby-contentful-starter)
 
 `)
