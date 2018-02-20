1d85d569c555d7a4782b1690880ee35ad2310238
stefan judis <stefanjudis@gmail.com>
2018-02-20 14:32:13 +0100

    Add import task

--- bin/hello.js
+++ bin/hello.js
@@ -9,9 +9,9 @@ ${chalk.green('Hey there! 👋')}
 Thanks for giving the ${pkg.name} a try. 🎉
 To get you going really quickly this project includes a setup step.
 
-${chalk.yellow('npm run import')} automates the following steps for you:
+${chalk.yellow.bold('npm run import')} automates the following steps for you:
   - imports ${chalk.green('a predefined content model')}
   - creates a config file ${chalk.yellow('.contentful.json')}
 
 When this is done run ${chalk.yellow('npm run dev')} and you're good to go.
-`);
+`)
