beb138678569068456a8e24876dea586395b6fba
Khaled Garbaya <khaled@contentful.com>
2018-05-17 14:00:37 +0200

    Remove previewToken

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,9 +7,7 @@ module.exports = {
     'gatsby-plugin-react-helmet',
     {
       resolve: 'gatsby-source-contentful',
-      options: process.env.NODE_ENV === 'development' ?
-        contentfulConfig.development :
-        contentfulConfig.production
+      options: contentfulConfig
     },
   ],
 }
