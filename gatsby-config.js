9aa51328c0e6be3738e168019e2cd08b0b67f918
Mudit Ameta <zeusdeux@gmail.com>
2018-05-23 15:28:37 +0200

    Add env var support

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,21 @@
-const contentfulConfig = require('./.contentful')
+let contentfulConfig
+
+try {
+  contentfulConfig = require('./.contentful')
+} catch (_) {
+  contentfulConfig = {
+    spaceId: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_DELIVERY_TOKEN,
+  }
+} finally {
+  const { spaceId, accessToken } = contentfulConfig
+
+  if (!spaceId || !accessToken) {
+    throw new Error(
+      'Contentful spaceId and the delivery token need to be provided.'
+    )
+  }
+}
 
 module.exports = {
   pathPrefix: '/gatsby-contentful-starter',
@@ -7,7 +24,7 @@ module.exports = {
     'gatsby-plugin-react-helmet',
     {
       resolve: 'gatsby-source-contentful',
-      options: contentfulConfig
+      options: contentfulConfig,
     },
   ],
 }
