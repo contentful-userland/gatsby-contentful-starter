658a7e24a8d9fdb670ed17036dee6d220b6e252a
Raymon S <raymonschouwenaar@users.noreply.github.com>
2018-08-20 19:08:22 +0200

    Updated gatsby-config because of an error at Netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,8 +7,8 @@ try {
 
 // Overwrite the Contentful config with environment variables if they exist
 contentfulConfig = {
-  spaceId: process.env.CONTENTFUL_SPACE_ID || contentfulConfig.spaceId,
-  accessToken: process.env.CONTENTFUL_DELIVERY_TOKEN || contentfulConfig.accessToken,
+  spaceId: process.env.CONTENTFUL_SPACE_ID || contentfulConfig.spaceId || '',
+  accessToken: process.env.CONTENTFUL_DELIVERY_TOKEN || contentfulConfig.accessToken || '',
 }
 
 const { spaceId, accessToken } = contentfulConfig
