7f1c418fe66b2b6b4c4eae80e364457f120a5af7
Joe Palmieri <jpalmieri@lumoslabs.com>
2018-10-07 14:21:45 -0700

    fix relative file paths

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import base from './base.css'
-import Container from '../components/container'
-import Navigation from '../components/navigation'
+import Container from './container'
+import Navigation from './navigation'
 
 class Template extends React.Component {
   render() {
