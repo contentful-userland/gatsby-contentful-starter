e0a3cef016a9041c712cab86487c9b802fa87d70
Joe Palmieri <jpalmieri@lumoslabs.com>
2018-11-12 16:45:50 -0800

    'npm' -> 'yarn' in hello script

--- bin/hello.js
+++ bin/hello.js
@@ -8,20 +8,20 @@ ${chalk.green('Hey there! 👋')}
 Thanks for giving the ${pkg.name} a try. 🎉
 To get you going really quickly this project includes a setup step.
 
-${chalk.yellow.bold('npm run setup')} automates the following steps for you:
+${chalk.yellow.bold('yarn run setup')} automates the following steps for you:
   - creates a config file ${chalk.yellow('./.contentful.json')}
   - imports ${chalk.green('a predefined content model')}
 
 When this is done run:
 
 ${chalk.yellow(
-  'npm run dev'
+  'yarn run dev'
 )} to start a development environment at ${chalk.green('localhost:8000')}
 
 or
 
 ${chalk.yellow(
-  'npm run build'
+  'yarn run build'
 )} to create a production ready static site in ${chalk.green('./public')}
 
 For further information check the readme of the project
