38edde98d4fd422352ee18477fa81cbb74ba1a2d
Skyler Shaw <skylerwshaw@gmail.com>
2020-02-28 14:14:40 +0000

    Update all dependencies (#66)
    * Update all dependencies
    
    * Applying all changes introduced with running 'yarn format' style linter

--- src/components/hero.js
+++ src/components/hero.js
@@ -5,7 +5,11 @@ import styles from './hero.module.css'
 
 export default ({ data }) => (
   <div className={styles.hero}>
-    <Img className={styles.heroImage} alt={data.name} fluid={data.heroImage.fluid} />
+    <Img
+      className={styles.heroImage}
+      alt={data.name}
+      fluid={data.heroImage.fluid}
+    />
     <div className={styles.heroDetails}>
       <h3 className={styles.heroHeadline}>{data.name}</h3>
       <p className={styles.heroTitle}>{data.title}</p>
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -3,7 +3,7 @@ import { Link, graphql } from 'gatsby'
 import get from 'lodash/get'
 import Helmet from 'react-helmet'
 import styles from './blog.module.css'
-import Layout from "../components/layout"
+import Layout from '../components/layout'
 import ArticlePreview from '../components/article-preview'
 
 class BlogIndex extends React.Component {
@@ -12,12 +12,10 @@ class BlogIndex extends React.Component {
     const posts = get(this, 'props.data.allContentfulBlogPost.edges')
 
     return (
-      <Layout location={this.props.location} >
+      <Layout location={this.props.location}>
         <div style={{ background: '#fff' }}>
           <Helmet title={siteTitle} />
-          <div className={styles.hero}>
-            Blog
-          </div>
+          <div className={styles.hero}>Blog</div>
           <div className="wrapper">
             <h2 className="section-headline">Recent articles</h2>
             <ul className="article-list">
