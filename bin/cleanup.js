8cf8d1672c4649a084b2d33aeb786b7016e7e2b6
dev-12868979-2a <dev-12868979-2a@inscoder.com>
2020-04-20 09:10:07 +0800

    feat: update JSON.stringify for adding line breaks and spacing for package.json

--- bin/cleanup.js
+++ bin/cleanup.js
@@ -8,5 +8,5 @@ delete packageJSON.scripts['setup']
 delete packageJSON.scripts['postinstall']
 delete packageJSON.scripts['cleanup-repository']
 
-let data = JSON.stringify(packageJSON)
+let data = JSON.stringify(packageJSON, null, 2)
 fs.writeFileSync(path.resolve(__dirname, '../package.json'), data)
